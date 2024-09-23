60040a203529ddcdd3dc0c863ea647698bbf00a8
michal <mnadrajkowski@gmail.com>
2024-09-23 19:18:08 +0200

    Ubuntu upload
    (cherry picked from commit cba377aa9a6be6d741bc2136a8c55193152e2e15)

--- src/main/java/com/gryzmol/App.java
+++ src/main/java/com/gryzmol/App.java
@@ -10,16 +10,16 @@ public class App {
         app.mainRun();
 
 
+
     }
 }
-/*
- - M:/Michal/Programowanie/Java/bookDiary/bookDiary/src/main/resources/Test.xlsx
- - ładna metoda to String
- - sprawdzić czy dodawane książki w trakcie działania programu zabisują się do db
- - przesuwanie książek musi sprawdzać czy dana książka juz nie jistnieje w zbiorze (sprawdzenie jest tylko w metodzie addNewBook
-- obsługa róznych formatów daty
-- sprawdzenie czy podane sa liczby
- - obsługa błędu przy niepoprawnych plikach excel w dodawaniu
+/*TODO
+   - ładna metoda to String
+   - sprawdzić czy dodawane książki w trakcie działania programu zabisują się do db
+   - przesuwanie książek musi sprawdzać czy dana książka juz nie jistnieje w zbiorze (sprawdzenie jest tylko w metodzie addNewBook
+   - sprawdzenie czy podane sa liczby
+   - obsługa błędu przy niepoprawnych plikach excel w dodawaniu
+   - Przesunąć try-cacth sprawdzenia daty w miejscu wywołania funkcji
 
 //w
 
