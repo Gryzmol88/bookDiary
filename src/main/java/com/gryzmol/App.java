26de973a3470c3b4e12de47d24bdf59e1c1333cd
Michu <mnadrajkowski@wp.pl>
2024-09-26 09:32:11 +0200

    Dodawanie ksiazki w czasie działania programu

--- src/main/java/com/gryzmol/App.java
+++ src/main/java/com/gryzmol/App.java
@@ -14,12 +14,9 @@ public class App {
     }
 }
 /*TODO
-   - sprawdzić czy dodawane książki w trakcie działania programu zabisują się do db
    - przesuwanie książek musi sprawdzać czy dana książka juz nie jistnieje w zbiorze (sprawdzenie jest tylko w metodzie addNewBook
    - sprawdzenie czy podane sa liczby
    - obsługa błędu przy niepoprawnych plikach excel w dodawaniu
    - Przesunąć try-cacth sprawdzenia daty w miejscu wywołania funkcji
-
-//w
 
  */
